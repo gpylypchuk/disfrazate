b35c215f45c56d600b5eaef959e3e951ad52258e
RAMIRO GRIN <ram8grin@gmail.com>
2024-11-19 23:19:08 -0300

    log in y eliminar cuenta

--- crud.py
+++ crud.py
@@ -77,8 +77,74 @@ def solicitar_contraseña():
         else:
             print("Contraseña no válida. Asegúrate de que tenga al menos 8 caracteres y contenga tanto letras como números.")
 
+# Funcion para obtener los datos del usuario
+def obtener_usuario(mail):
+    # Consulta para obtener el usuario
+    query = 'SELECT * FROM clientes WHERE mail = %s;'
+    # Ejecutar la consulta y pasar los parámetros de manera segura
+    cursor.execute(query, (mail))
+    # Obtener el resultado
+    datos_disfraz = cursor.fetchone()  # Usar fetchone() para una sola fila
+    # Verifico si se encontro el disfraz
+    if datos_disfraz:
+        print(f"Disfraz encontrado: {datos_disfraz}")
+    else:
+        print("No se encontró el disfraz con ese ID.")
+    # Devuelvo los datos
+    return datos_disfraz
 
 ''' FUNCIONES ESPECIFICAS '''
+'''USUARIO'''
+# Funcion para dar de alta un nuevo usuario
+def nuevo_usuario():
+    # Solicitamos y validamos los datos del usuario
+    nombre = input("Introduce tu nombre: ")
+    apellido = input("Introduce tu apellido: ")
+    dni = solicitar_dni()
+    email = solicitar_email()
+    telefono = solicitar_telefono()
+    password = solicitar_contraseña()
+    
+    # GUARDAMOS LOS DATOS EN LA BD
+    try:
+        # Consulta SQL con parámetros para evitar inyección SQL
+        query = """
+            INSERT INTO Usuarios (dni, name, apellidos, email, telefono, contrasena) 
+            VALUES (%s, %s, %s, %s, %s, %s)
+        """
+        # Ejecutar la consulta con los valores correspondientes
+        cursor.execute(query, (dni, nombre, apellido, email, telefono, password))
+        connection.commit()
+    except Exception as e:
+        # Si ocurre un error, se hace rollback
+        connection.rollback()
+        print(f"Error al insertar los datos: {e}")
+
+# Funcion para dar de baja un usuario
+def eliminar_usuario():
+    try:
+        # Solicitamos el DNI del usuario
+        dni = solicitar_dni()
+        # Definimos la consulta SQL para eliminar al usuario
+        query = "DELETE FROM clientes WHERE dni = %s"
+        # Ejecutamos la consulta con el DNI proporcionado
+        cursor.execute(query, (dni,))
+        # Confirmamos los cambios en la base de datos
+        connection.commit()
+        print(f'Usuario con DNI {dni} eliminado correctamente.')
+    except Exception as e:
+        # Si ocurre un error, hacemos rollback y mostramos el error
+        connection.rollback()
+        print(f'Error al eliminar el usuario: {e}')
+
+# Funcion para iniciar sesion
+def iniciar_sesion():
+    email = solicitar_email()
+    password = solicitar_contraseña()
+    datos_usuario = obtener_usuario(email)
+    print('Los datos del usuario son: ',datos_usuario)
+
+'''DISFRACES'''
 # Función para que el cliente compre un disfraz
 def comprar_disfraz():  
     while True:
@@ -147,31 +213,6 @@ def alquilar_disfraz():
         finally:
             break
 
-# Funcion para dar de alta un nuevo usuario
-def nuevo_usuario():
-    # Solicitamos y validamos los datos del usuario
-    nombre = input("Introduce tu nombre: ")
-    apellido = input("Introduce tu apellido: ")
-    dni = solicitar_dni()
-    email = solicitar_email()
-    telefono = solicitar_telefono()
-    password = solicitar_contraseña()
-    
-    # GUARDAMOS LOS DATOS EN LA BD
-    try:
-        # Consulta SQL con parámetros para evitar inyección SQL
-        query = """
-            INSERT INTO Usuarios (dni, name, apellidos, email, telefono, contrasena) 
-            VALUES (%s, %s, %s, %s, %s, %s)
-        """
-        # Ejecutar la consulta con los valores correspondientes
-        cursor.execute(query, (dni, nombre, apellido, email, telefono, password))
-        connection.commit()
-    except Exception as e:
-        # Si ocurre un error, se hace rollback
-        connection.rollback()
-        print(f"Error al insertar los datos: {e}")
-
 ''' MENU PRINCIPAL COMPRA Y USUARIO'''
 def menu_principal_usuario():
     while True:
@@ -190,9 +231,9 @@ def menu_principal_usuario():
         if opcion == 1:
             nuevo_usuario()
         elif opcion == 2:
-            print("Iniciar sesion")
+            iniciar_sesion()
         elif opcion == 3:
-            print("Eliminamos cuenta")
+            eliminar_usuario()
         elif opcion == 4:
             print("Gracias por utilizar el sistema. ¡Hasta pronto!")
             break
